d259028ac41766a7da4858a376c863066d9a781b
myonlystarWang <54332782@qq.com>
2024-03-14 17:14:40 +0800

    修改代码风格和bug

--- calculator.py
+++ calculator.py
@@ -203,7 +203,7 @@ def all_skill_gains_calculate(my_attributes, skill_gains_para, var_gains_para):
                                       my_attributes.get("主输出_1%真气比面板真气", 0) * (number + skill_gains_para.get("技能增益_真气比_五气朝元", 0)) + \
                                       var_gains_para.get("墨雪特效霜情", 0) + \
                                       200 + number * 100
-        
+
         skill_gains_para["技能增益_攻击比_天魔附体"] = 15 + 5 + round(all_zhenqi / 50000, 1)
         #skill_gains_para["技能增益_自身防御力"] = my_attributes.get("主输出_自身防御力", 0)
         skill_gains_para["技能增益_自身防御力_枯木咒"] = fy_chiqing_dict.get(my_attributes.get("主输出_玄烛品质_痴情咒", 2.5), 2.5)
@@ -265,21 +265,17 @@ def my_gained_attribute_calculate(my_attributes, skill_gains_para, var_gains_par
         # 根据不同名目进行赋值
         if  key == "主输出_爆伤":
             bs_sum = sum(skill_categories["爆伤"].values())
-            new_value = value + bs_sum
+            new_value = min(value + bs_sum, 3000)
             
-            if new_value > 3000:
-                new_value = 3000
-
         elif key == "主输出_真气":
             zqb_sum = sum(skill_categories["真气比"].values())
             zq_sum = sum(skill_categories["真气"].values())
             new_value =  value + zq_sum + zqb_sum * my_attributes.get("主输出_1%真气比面板真气", 0)
             # 不是鬼王则需要限制真气
             if my_attributes.get("主输出_职业", 0) != 1:
-                if new_value > 4000000:
-                    new_value = 4000000
-                
-        elif "最大攻击" in key:       
+                new_value = min(new_value, 4000000)
+       
+        elif "最大攻击" in key or "最小攻击" in key:                
             # 攻击计算
             gjb_sum = sum(skill_categories["攻击比"].values())
             gj_sum = sum(skill_categories["攻击"].values())
@@ -287,7 +283,7 @@ def my_gained_attribute_calculate(my_attributes, skill_gains_para, var_gains_par
             # 主输出是鬼王需要额外计算攻击
             if my_attributes.get("主输出_职业", 0) == 1:
                 # 第一轮枯木前的最大攻击
-                before1_kumu_gj = value + \
+                before1_kumu_gj = min(value + \
                                 skill_gains_para.get("技能增益_攻击_副本赠送", 0) + \
                                 skill_gains_para.get("技能增益_攻击_龙虎1", 0) + \
                                 skill_gains_para.get("技能增益_攻击_龙虎之力", 0) + \
@@ -302,11 +298,11 @@ def my_gained_attribute_calculate(my_attributes, skill_gains_para, var_gains_par
                                  skill_gains_para.get("技能增益_攻击比_家族技能", 0) + \
                                  skill_gains_para.get("技能增益_攻击比_龙虎1", 0) + \
                                  skill_gains_para.get("技能增益_攻击比_龙虎之力", 0) 
-                                ) * my_attributes.get("主输出_1%攻击比面板攻击", 0)
+                                ) * my_attributes.get("主输出_1%攻击比面板攻击", 0), 750000)
                 print("before1_kumu_gj", before1_kumu_gj)
 
                 # 第一轮枯木后的防御，枯木算的是自身防御的3倍，自身防御是所有吃百分比增益的防御固定值
-                after1_kumu_fy = my_attributes.get("主输出_防御", 0) + \
+                after1_kumu_fy = min(my_attributes.get("主输出_防御", 0) + \
                                 skill_gains_para.get("技能增益_自身防御力_枯木咒", 0) * my_attributes.get("主输出_自身防御力", 0) + \
                                 skill_gains_para.get("技能增益_防御_龙虎1", 0) + \
                                 skill_gains_para.get("技能增益_防御_龙虎之力", 0) + \
@@ -314,11 +310,11 @@ def my_gained_attribute_calculate(my_attributes, skill_gains_para, var_gains_par
                                  skill_gains_para.get("技能增益_防御比_龙虎之力", 0) + \
                                  skill_gains_para.get("技能增益_防御比_五行八卦", 0) + \
                                  skill_gains_para.get("技能增益_防御比_雪琪的祈愿", 0)
-                                 ) * my_attributes.get("主输出_1%防御比面板防御", 0)
+                                 ) * my_attributes.get("主输出_1%防御比面板防御", 0), 500000)
                 print("after1_kumu_fy", after1_kumu_fy)
 
                 # 第一轮锐金后的最大攻击
-                after1_ruijin_gj = before1_kumu_gj + min(after1_kumu_fy, before1_kumu_gj * skill_gains_para.get("技能增益_攻击系数_锐金咒", 0))
+                after1_ruijin_gj = min(before1_kumu_gj + min(after1_kumu_fy, before1_kumu_gj * skill_gains_para.get("技能增益_攻击系数_锐金咒", 0)), 750000)
                 print("after1_ruijin_gj", after1_ruijin_gj)
                 
                 # 第二轮枯木/万法后的满状态防御，筛选金刚护体1 2，秋声之间的最大值
@@ -326,114 +322,31 @@ def my_gained_attribute_calculate(my_attributes, skill_gains_para, var_gains_par
                 present_skills = [skill for skill in specific_skills if skill in skill_categories["防御"]]
                 
                 # 如果存在，找出最大值
-                if present_skills:
-                    max_value = max([skill_categories["防御"][skill] for skill in present_skills])
-                else:
-                    max_value = 0
+                max_value = max([
+                    skill_categories["防御"][skill] for skill in present_skills
+                ]) if present_skills else 0
 
                 # 计算总和，不包括其他特定的技能值，还需要加上枯木或万法的3倍自身防御增益
                 fy_sum = sum([skill_value for skill, skill_value in skill_categories["防御"].items() if skill not in specific_skills]) + max_value + 3 * my_attributes.get("主输出_自身防御力", 0)
                 fyb_sum = sum(skill_categories["防御比"].values())
-                after2_kumu_fy = my_attributes.get("主输出_防御", 0) + fy_sum + fyb_sum * my_attributes.get("主输出_1%防御比面板防御", 0)
-                print(fy_sum)
-                print(fyb_sum)
+                after2_kumu_fy = min(my_attributes.get("主输出_防御", 0) + fy_sum + fyb_sum * my_attributes.get("主输出_1%防御比面板防御", 0), 500000)
                 print("after2_kumu_fy", after2_kumu_fy)
 
                 # 第二轮枯木后锐金前的满状态最大攻击
-                before2_ruijin_gj = value + sum(skill_categories["攻击"].values()) + sum(skill_categories["攻击比"].values()) * my_attributes.get("主输出_1%攻击比面板攻击", 0)
+                before2_ruijin_gj = min(value + sum(skill_categories["攻击"].values()) + sum(skill_categories["攻击比"].values()) * my_attributes.get("主输出_1%攻击比面板攻击", 0), 750000)
                 print("before2_ruijin_gj", before2_ruijin_gj)
 
                 # 第二轮枯木后锐金后的满状态最大攻击
-                after2_ruijin_gj = before2_ruijin_gj + min(after2_kumu_fy, before2_ruijin_gj * skill_gains_para.get("技能增益_攻击系数_锐金咒", 0))
+                after2_ruijin_gj = min(before2_ruijin_gj + min(after2_kumu_fy, before2_ruijin_gj * skill_gains_para.get("技能增益_攻击系数_锐金咒", 0)), 750000)
                 print("after2_ruijin_gj", after2_ruijin_gj)
 
                 # 第二轮猛火后的满状态最大攻击
-                after2_menghuo_gj = int(after2_ruijin_gj * (1 + skill_gains_para.get("技能增益_猛火咒3", 0)))
+                after2_menghuo_gj = min(int(after2_ruijin_gj * (1 + skill_gains_para.get("技能增益_猛火咒3", 0))), 750000)
                 print("after2_menghuo_gj", after2_menghuo_gj)
                 new_value = after2_menghuo_gj
 
             else:
-                new_value = value + gj_sum + gjb_sum * my_attributes.get("主输出_1%攻击比面板攻击", 0)
-                 
-            if new_value > 750000:
-                new_value = 750000
-
-        elif "最小攻击" in key:
-            # 攻击计算
-            gjb_sum = sum(skill_categories["攻击比"].values())
-            gj_sum = sum(skill_categories["攻击"].values())
-
-            # 主输出是鬼王需要额外计算攻击
-            if my_attributes.get("主输出_职业", 0) == 1:
-                # 第一轮枯木前的最小攻击
-                before1_kumu_gj = value + \
-                                skill_gains_para.get("技能增益_攻击_副本赠送", 0) + \
-                                skill_gains_para.get("技能增益_攻击_龙虎1", 0) + \
-                                skill_gains_para.get("技能增益_攻击_龙虎之力", 0) + \
-                                skill_gains_para.get("技能增益_攻击_天罡战意", 0) + \
-                                skill_gains_para.get("技能增益_攻击_家族技能", 0) + \
-                                (skill_gains_para.get("技能增益_攻击比_金蛇狂舞", 0) + \
-                                 skill_gains_para.get("技能增益_攻击比_鸣泉雅韵", 0) + \
-                                 skill_gains_para.get("技能增益_攻击比_天魔附体", 0) + \
-                                 skill_gains_para.get("技能增益_攻击比_天罡伏魔", 0) + \
-                                 skill_gains_para.get("技能增益_攻击比_豪血", 0) + \
-                                 skill_gains_para.get("技能增益_攻击比_灵雨续春2", 0) + \
-                                 skill_gains_para.get("技能增益_攻击比_家族技能", 0) + \
-                                 skill_gains_para.get("技能增益_攻击比_龙虎1", 0) + \
-                                 skill_gains_para.get("技能增益_攻击比_龙虎之力", 0) 
-                                ) * my_attributes.get("主输出_1%攻击比面板攻击", 0)
-                print("before1_kumu_gj", before1_kumu_gj)
-
-                # 第一轮枯木后的防御，枯木算的是自身防御的3倍，自身防御是所有吃百分比增益的防御固定值
-                after1_kumu_fy = my_attributes.get("主输出_防御", 0) + \
-                                skill_gains_para.get("技能增益_自身防御力_枯木咒", 0) * my_attributes.get("主输出_自身防御力", 0) + \
-                                skill_gains_para.get("技能增益_防御_龙虎1", 0) + \
-                                skill_gains_para.get("技能增益_防御_龙虎之力", 0) + \
-                                (skill_gains_para.get("技能增益_防御比_龙虎1", 0) + \
-                                 skill_gains_para.get("技能增益_防御比_龙虎之力", 0) + \
-                                 skill_gains_para.get("技能增益_防御比_五行八卦", 0) + \
-                                 skill_gains_para.get("技能增益_防御比_雪琪的祈愿", 0)
-                                 ) * my_attributes.get("主输出_1%防御比面板防御", 0)
-                print("after1_kumu_fy", after1_kumu_fy)
-
-                # 第一轮锐金后的最大攻击
-                after1_ruijin_gj = before1_kumu_gj + min(after1_kumu_fy, before1_kumu_gj * skill_gains_para.get("技能增益_攻击系数_锐金咒", 0))
-                print("after1_ruijin_gj", after1_ruijin_gj)
-                
-                # 第二轮枯木/万法后的满状态防御，筛选金刚护体1 2，秋声之间的最大值
-                specific_skills = ["技能增益_防御_金刚不坏", "技能增益_防御_金刚不坏2", "技能增益_防御_秋声雅韵"]
-                present_skills = [skill for skill in specific_skills if skill in skill_categories["防御"]]
-                
-                # 如果存在，找出最大值
-                if present_skills:
-                    max_value = max([skill_categories["防御"][skill] for skill in present_skills])
-                else:
-                    max_value = 0
-
-                # 计算总和，不包括其他特定的技能值，还需要加上枯木或万法的3倍自身防御增益
-                fy_sum = sum([skill_value for skill, skill_value in skill_categories["防御"].items() if skill not in specific_skills]) + max_value + 3 * my_attributes.get("主输出_自身防御力", 0)
-                fyb_sum = sum(skill_categories["防御比"].values())
-                after2_kumu_fy = my_attributes.get("主输出_防御", 0) + fy_sum + fyb_sum * my_attributes.get("主输出_1%防御比面板防御", 0)
-                print("after2_kumu_fy", after2_kumu_fy)
-
-                # 第二轮枯木后锐金前的满状态最大攻击
-                before2_ruijin_gj = value + sum(skill_categories["攻击"].values()) + sum(skill_categories["攻击比"].values()) * my_attributes.get("主输出_1%攻击比面板攻击", 0)
-                print("before2_ruijin_gj", before2_ruijin_gj)
-
-                # 第二轮枯木后锐金后的满状态最大攻击
-                after2_ruijin_gj = before2_ruijin_gj + min(after2_kumu_fy, before2_ruijin_gj * skill_gains_para.get("技能增益_攻击系数_锐金咒", 0))
-                print("after2_ruijin_gj", after2_ruijin_gj)
-
-                # 第二轮猛火后的满状态最大攻击
-                after2_menghuo_gj = int(after2_ruijin_gj * (1 + skill_gains_para.get("技能增益_猛火咒3", 0)))
-                print("after2_menghuo_gj", after2_menghuo_gj)
-                new_value = after2_menghuo_gj
-
-            else:
-                new_value = value + gj_sum + gjb_sum * my_attributes.get("主输出_1%攻击比面板攻击", 0)
-
-            if new_value > 750000:
-                new_value = 750000
+                new_value = min(value + gj_sum + gjb_sum * my_attributes.get("主输出_1%攻击比面板攻击", 0), 750000)
 
         elif key == "主输出_防御":
             # 筛选金刚护体1 2，秋声之间的最大值
@@ -441,28 +354,20 @@ def my_gained_attribute_calculate(my_attributes, skill_gains_para, var_gains_par
             present_skills = [skill for skill in specific_skills if skill in skill_categories["防御"]]
             
             # 如果存在，找出最大值
-            if present_skills:
-                max_value = max([skill_categories["防御"][skill] for skill in present_skills])
-            else:
-                max_value = 0
+            max_value = max([skill_categories["防御"][skill] for skill in present_skills]) if present_skills else 0
 
             # 计算总和，不包括其他特定的技能值，还需要加上枯木或万法的3倍自身防御增益
             fy_sum = sum([value for skill, value in skill_categories["防御"].items() if skill not in specific_skills]) + max_value + 3 * my_attributes.get("主输出_自身防御力", 0)
             fyb_sum = sum(skill_categories["防御比"].values())
-            new_value = value + fy_sum + fyb_sum * my_attributes.get("主输出_1%防御比面板防御", 0)
+            new_value = min(value + fy_sum + fyb_sum * my_attributes.get("主输出_1%防御比面板防御", 0), 500000)
 
-            # 设定防御上限为50万
-            if new_value > 500000:
-                new_value = 500000
-                
         elif key == "主输出_气血":
             qxb_sum = sum(skill_categories["气血比"].values())
             qx_sum = sum(skill_categories["气血"].values())
             new_value = value + qx_sum + qxb_sum * my_attributes.get("主输出_1%气血比面板气血", 0)
             
             if my_attributes.get("主输出_职业", 0) != 1:
-                if new_value > 4000000:
-                    new_value = 4000000
+                new_value = min(new_value, 4000000)
         else:
             # 其他属性保持不变
             new_value = value
@@ -489,9 +394,7 @@ def calculate_basic_damage(my_gain_attributes, boss_attributes, roles_para, skil
         elif "爆伤" in key:
             skill_bs = value
 
-    # 计算攻击比和固定攻击总增益
-            
-    #        
+    # 计算基础伤害
     attack_min = my_gain_attributes.get("主输出_最小攻击", 0) * attack_coeff + attack_fixed
     attack_max = my_gain_attributes.get("主输出_最大攻击", 0) * attack_coeff + attack_fixed
 
